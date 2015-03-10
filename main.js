a4fb9ae8062f22dc167d85c0300e1863d52866c6
Peter Verzijl <p.verzijl@student.utwente.nl>
2015-03-11 00:07:33 +0100

    Changed to css3 animations

--- main.js
+++ main.js
@@ -1,54 +1,28 @@
 $(document).ready(function () {
 
-  initialize();
-
-  // Animate pulse
+  // Create pulse at mouse position
   $(this).click(function(e) {
     var x = e.clientX;
     var y = e.clientY;
     createPulse( x, y );
   });
+  
 });
 
 $( window ).resize(function() {
-  initialize();
+
 });
-
-// Centers and recalculates screen
-var initialize = function() {
-
-  // Resize
-  $('.face').css({
-    height : $('.face').width()
-  });
-
-  // Center
-  $('.circle').css({
-    marginLeft: -$('.circle').width()/2,
-    marginTop: -$('.circle').height()/2
-  });
-  // Center .center class
-  $('.center').css({
-    marginLeft: -$('.center').width()/2
-  });
-}
 
 // Creates a pulse at x,y
 var createPulse = function(x, y) {
 
-  var circle = $("<div class='circle'></div>");
+  var circle = $("<div class='circle animate'></div>");
   $('body').append(circle)
   circle.css({
     top : y,
     left : x,
     'z-index' : 0
-  })
-  .animate({
-    width : 4000,
-    height : 4000,
-    marginLeft : -4000/2,
-    marginTop : -4000/2
-  }, 1000, function() {
+  }).bind('oanimationend animationend webkitAnimationEnd', function() {
     $(this).remove();
   });
 }
